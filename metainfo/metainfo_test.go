f1f0aa9b48110d23b1eb632922968ea75d1bc4bb
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2024-11-18 01:10:43 -0500

    Test metainfo parsing

---
+++ metainfo/metainfo_test.go
@@ -0,0 +1,22 @@
+package metainfo
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestArtOfWar(t *testing.T) {
+	testFile("artofwar.torrent", t)
+}
+
+func TestHitchhikers(t *testing.T) {
+	testFile("hitchhikersguide.torrent", t)
+}
+
+func testFile(name string, t *testing.T) {
+	mi, err := UnmarshalMetainfo(name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%#v\n", mi)
+}
