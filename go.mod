dd44842c014435a1e92434b184342b4387b2cec3
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2024-11-18 00:19:52 -0500

    Draft metainfo file parsing for single file mode

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/zaheenadeep/torrgo
 
 go 1.23.2
+
+require github.com/anacrolix/torrent v1.57.1
+
+require (
+	github.com/anacrolix/missinggo v1.3.0 // indirect
+	github.com/anacrolix/missinggo/v2 v2.7.4 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+)
---
+++ metainfo/metainfo.go
@@ -0,0 +1,39 @@
+package metainfo
+
+import (
+	"os"
+	"fmt"
+	"github.com/anacrolix/torrent/bencode"
+)
+
+type Metainfo struct {
+	Info         struct {
+		PieceLength int    `bencode:"piece length"`
+		Pieces      string `bencode:"pieces"`
+		Private     int    `bencode:"private,omitempty"`
+		Name        string `bencode:"name"`
+		Length      int    `bencode:"length"`
+		MD5Sum      string `bencode:"md5sum"`
+	}                       `bencode:"info"`
+	Announce     string     `bencode:"announce"`
+	AnnounceList [][]string	`bencode:"announce-list,omitempty"`
+	CreationDate int        `bencode:"creation date,omitempty"`
+	Comment      string     `bencode:"comment,omitempty"`
+	CreatedBy    string     `bencode:"created by,omitempty"`
+	Encoding     string     `bencode:"encoding,omitempty"`
+}
+
+func UnmarshalMetainfo(name string) (*Metainfo, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, fmt.Errorf("os.ReadFile: %v", err)
+	}
+
+	var mi Metainfo
+	err = bencode.Unmarshal(data, &mi)
+	if err != nil {
+		return nil, fmt.Errorf("bencode.Unmarshal: %v", err)
+	}
+
+	return &mi, nil
+}
