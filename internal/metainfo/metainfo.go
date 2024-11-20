c190a8f8fac395e4a0739493bb445a32bc6923b4
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2024-11-19 21:39:01 -0500

    Start adding multi file support

--- internal/metainfo/metainfo.go
+++ internal/metainfo/metainfo.go
@@ -6,23 +6,37 @@ import (
 	"github.com/anacrolix/torrent/bencode"
 )
 
-type InfoDict struct {
+type SingleFileInfo struct {
 	PieceLength int    `bencode:"piece length"`
 	Pieces      string `bencode:"pieces"`
 	Private     int    `bencode:"private,omitempty"`
 	Name        string `bencode:"name"`
 	Length      int    `bencode:"length"`
-	MD5Sum      string `bencode:"md5sum"`
+	MD5Sum      string `bencode:"md5sum,omitempty"`
+}
+
+type MIFile struct {
+	Length      int      `bencode:"length"`
+	MD5Sum      string   `bencode:"md5sum,omitempty"`
+	Path        []string `bencode:"path"`
+}
+
+type MultiFileInfo struct {
+	PieceLength int      `bencode:"piece length"`
+	Pieces      string   `bencode:"pieces"`
+	Private     int      `bencode:"private,omitempty"`
+	Name        string   `bencode:"name"`
+	Files       []MIFile `bencode:"md5sum"`
 }
 
 type Metainfo struct {
-	Info         InfoDict   `bencode:"info"`
-	Announce     string     `bencode:"announce"`
-	AnnounceList [][]string	`bencode:"announce-list,omitempty"`
-	CreationDate int        `bencode:"creation date,omitempty"`
-	Comment      string     `bencode:"comment,omitempty"`
-	CreatedBy    string     `bencode:"created by,omitempty"`
-	Encoding     string     `bencode:"encoding,omitempty"`
+	InfoBytes    bencode.Bytes `bencode:"info"`
+	Announce     string        `bencode:"announce"`
+	AnnounceList [][]string	   `bencode:"announce-list,omitempty"`
+	CreationDate int           `bencode:"creation date,omitempty"`
+	Comment      string        `bencode:"comment,omitempty"`
+	CreatedBy    string        `bencode:"created by,omitempty"`
+	Encoding     string        `bencode:"encoding,omitempty"`
 }
 
 func UnmarshalMetainfo(name string) (*Metainfo, error) {
