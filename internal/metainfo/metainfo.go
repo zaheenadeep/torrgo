6598c87c560f3d3096b578b2e32c147eab7efd9a
Zaheen Ahmed Deep <zaheen.a.deep@gmail.com>
2024-12-21 18:33:32 -0500

    Change info dict parsing

--- internal/metainfo/metainfo.go
+++ internal/metainfo/metainfo.go
@@ -3,38 +3,32 @@ package metainfo
 import (
 	"os"
 	"fmt"
-	"strings"
 	"crypto/sha1"
 	"github.com/anacrolix/torrent/bencode"
 )
 
-type SingleFileInfo struct {
-	PieceLength int    `bencode:"piece length"`
-	Pieces      []byte `bencode:"pieces"`
-	Private     int    `bencode:"private,omitempty"`
-	Name        string `bencode:"name"`
-	Length      int    `bencode:"length"`
-	MD5Sum      string `bencode:"md5sum,omitempty"`
-}
-
-type MIFile struct {
+type FileInfo struct {
 	Length      int      `bencode:"length"`
 	MD5Sum      string   `bencode:"md5sum,omitempty"`
 	Path        []string `bencode:"path"`
 }
 
-type MultiFileInfo struct {
-	PieceLength int      `bencode:"piece length"`
-	Pieces      []byte   `bencode:"pieces"`
-	Private     int      `bencode:"private,omitempty"`
-	Name        string   `bencode:"name"`
-	Files       []MIFile `bencode:"files"`
+type Info struct {
+	PieceLength int    `bencode:"piece length"`
+	Pieces      []byte `bencode:"pieces"`
+	Private     int    `bencode:"private,omitempty"`
+	Name        string `bencode:"name"`
+
+	// Single File Info Only
+	Length      int    `bencode:"length,omitempty"`  // mandatory
+	MD5Sum      string `bencode:"md5sum,omitempty"`  // optional
+
+	// Multi File Info Only
+	Files       []FileInfo `bencode:"files,omitempty"` // mandatory
 }
 
-type FileInfo interface{}
-
 type Metainfo struct {
-	Info         FileInfo
+	Info         Info
 	InfoBytes    bencode.Bytes `bencode:"info"`
 	Announce     string        `bencode:"announce"`
 	AnnounceList [][]string	   `bencode:"announce-list,omitempty"`
@@ -56,26 +50,21 @@ func Load(name string) (*Metainfo, error) {
 		return nil, fmt.Errorf("bencode.Unmarshal: %v", err)
 	}
 
-	if strings.Contains(mi.InfoBytes.GoString(), ":pathl") {
-		var m MultiFileInfo
-		err = bencode.Unmarshal(mi.InfoBytes, &m)
-		if err != nil {
-			return nil, fmt.Errorf("bencode.Unmarshal: %v", err)
-		}
-		mi.Info = m
+	err = bencode.Unmarshal(mi.InfoBytes, &mi.Info)
+	if err != nil {
+		return nil, fmt.Errorf("bencode.Unmarshal: %v", err)
+	} else if mi.Info.Files != nil && mi.Info.Length != 0 { // TODO: 0 byte file?
+		return nil, fmt.Errorf("Info dict has both files and length keys")
 	} else {
-		var s SingleFileInfo
-		err = bencode.Unmarshal(mi.InfoBytes, &s)
-		if err != nil {
-			return nil, fmt.Errorf("bencode.Unmarshal: %v", err)
-		}
-		mi.Info = s
+		return &mi, nil
 	}
-
-	return &mi, nil
 }
 
 func (mi *Metainfo) InfoHash() []byte {
 	b := sha1.Sum(mi.InfoBytes)
 	return b[:]
+}
+
+func (mi *Metainfo) TotalSize() int {
+	return mi.Info.PieceLength * len(mi.Info.Pieces)
 }
